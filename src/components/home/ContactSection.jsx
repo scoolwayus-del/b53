4192e434e7d279f018521ec0557b3d49d6818389
scoolwayus-del <scoolway.us@gmail.com>
2025-10-06 16:54:23 +0530

    Updated ContactSection.jsx

--- src/components/home/ContactSection.jsx
+++ src/components/home/ContactSection.jsx
@@ -65,7 +65,7 @@ const ContactSection = () => {
   const socialLinks = [
     { name: 'Instagram', url: 'https://instagram.com/s111khar', icon: '/icons8-instagram-100.png' },
     { name: 'Facebook', url: 'https://https://www.facebook.com/profile.php?id=61581449278227', icon: '/icons8-facebook-100.png' },
-    { name: 'LinkedIn', url: 'https://linkedin.com/company/amouraworks', icon: '/icons8-linkedin-100.png' },
+    { name: 'LinkedIn', url: 'https://linkedin.com/company/amoura-works', icon: '/icons8-linkedin-100.png' },
   ];
 
   return (
